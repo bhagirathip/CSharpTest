55c68616343e3248d2746e08f77423e8daedac9f
Bhagirathi Panda <bhagirathi.toxic@gmail.com>
2017-06-12 16:03:05 +0530

    First Commit

---
+++ CSharpTest/CSharp6Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp6Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 25;
+            int x = 5;
+            double myHourlyRate = 150.00;
+            var myName = "test";
+            Console.WriteLine($"myAge: {myAge}, x:{x}, myHourlyRate {myHourlyRate}");
+            Console.ReadLine();
+        }
+    }
+}
